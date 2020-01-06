7edb96d54a183d5f6615c2f7239e29bcfcd727d6
Joyce <wamungai29@gmail.com>
2020-01-06 08:42:34 +0300

    Update the blueprint by importing files then define route decorators using the main blueprint instance.

---
+++ app/main/views.py
@@ -0,0 +1,24 @@
+from flask import render_template,request,redirect,url_for 
+from .import main
+from .. request import get_source,get_articles
+from ..model import Source
+
+@main.route('/')
+def index():
+    general_news =get_source('general')
+    print('*************general news*********************')
+    print('general_news')
+    
+    title='Latest News'
+    
+    return render_template('index.html,title = title,general=general_news')
+
+@main.route('/article/<int:id>')
+
+def article(id):
+    article =get_articles(id)
+    print('*************get_article****************')
+    print(article)
+    
+    title = f'{id}'
+    return render_template(trial.html, title = title, article= article)
