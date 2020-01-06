51ad7f039674fb009e5a74b545dcfe8d5c4d9ab3
Joyce <wamungai29@gmail.com>
2020-01-06 22:28:24 +0300

    Import operating system module and create dictionary.

---
+++ app/config.py
@@ -0,0 +1,34 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SOURCE_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLE_BASE_URL='https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    API_KEY= os.environ.get('API_KEY')
+    
+    class ProdConfig(Config):
+        '''
+        Production configuration child class
+        
+        Args:
+            Config:The parent configuration class with General congiration settings
+        '''
+        pass
+    
+    class DevConfig(onfig):
+        '''
+        Development configuration child class
+        
+        Args:
+            Config:The parent configuration class with General configuration settings
+        '''
+        
+        DEBUG = True
+        
+    config_options = {
+        'development':DevConfig,
+        'production':ProdConfig
+    }
+        
+    
